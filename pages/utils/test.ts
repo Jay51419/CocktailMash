7855a5a6a45f9d14b9c2cfc84b5fb5e584dc137c
jay51419 <jaygandhi51419@gmail.com>
2022-10-23 01:02:50 -0700

    removed unused modules

--- pages/utils/test.ts
+++
@@ -1,5 +0,0 @@
-import { Extrapolate, interpolate } from "./interpolate";
-const activeIndex = 0 
-const outputRange = [-350,0,350]
-const val = interpolate(activeIndex,{inputRange:[0,1,2],outputRange,extrapolate:Extrapolate.clamp})
-console.log(val)
