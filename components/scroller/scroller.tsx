dc310766b5ab4a63b649f6cfd1c3d11c2e327b53
jay51419 <jaygandhi51419@gmail.com>
2022-11-23 15:26:10 +0530

    Updated Scrollbar in scroller.tsx

--- components/scroller/scroller.tsx
+++ components/scroller/scroller.tsx
@@ -1,45 +1,29 @@
-import React, {ReactNode, useCallback, useEffect, useRef, useState,} from "react";
-import {useMediaQuery} from "@mantine/hooks";
+import React, { ReactNode, useCallback, useEffect, useRef, useState, } from "react";
+import { useMediaQuery } from "@mantine/hooks";
+import Scrollbar from 'smooth-scrollbar';
 
 interface ScrollerProps {
     children: ReactNode
 }
-export default function Scroller({children}: ScrollerProps) {
-    const isMobile = useMediaQuery("(pointer:none), (pointer:coarse)")
+export default function Scroller({ children }: ScrollerProps) {
     const ref = useRef<HTMLDivElement>(null)
-    const [transform3D, setTransform3D] = useState<string>(`translate3d(0px,0px, 0px)`)
-
-    const easeScroll = useCallback(()=> {
-        const x = window.scrollX
-        const y = window.scrollY;
-        setTransform3D(`translate3d(-${x}px, -${y}px, 0px)`)
-    },[setTransform3D])
-
-
     useEffect(() => {
-        window.addEventListener('scroll', easeScroll);
+        const scrollbar = Scrollbar.init(ref.current!, { damping: 0.07, alwaysShowTracks: true })
         return () => {
-            window.removeEventListener("scroll", easeScroll)
-        }
-    }, [easeScroll])
-
+            scrollbar.destroy();
+        };
+    }, [])
 
     return <div
-        onLoad={() => {
-            const body = document.body;
-            body.style.height = ref.current?.clientHeight + 'px'
-        }
-        }
         ref={ref}
+        data-scrollbar
         style={{
-            position: "fixed",
-            top: 0,
-            left: 0,
+            height: "100vh",
             width: "100%",
-            transform: transform3D,
-            transition:`transform ${isMobile?0:0.3}s linear`
         }}>
-        {children}
+        <div >
+            {children}
+        </div>
     </div>
 }
 
